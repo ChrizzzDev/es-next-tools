9e17f30a62f8abea66df94c76624c58a747f6048
ChrizzzDev <chrizdev4@gmail.com>
2025-01-09 08:17:46 -0600

    fix: Fixed a problem with types where the values ​​were not returned correctly.

--- src/object/flatten.ts
+++ src/object/flatten.ts
@@ -1,24 +1,27 @@
-export type FlattenedObject = { [key: string]: any };
+export type FlattenedObject<T> = {
+  [K in keyof T]?: T[K] extends object
+    ? FlattenedObject<T[K]>
+    : T[K];
+} & { [key: string]: any };
 
 /**
  * Flattens a nested object structure.
- * @param {Record<string, any>} obj - The object to flatten.
+ * @param {T} obj - The object to flatten.
  * @param {string} [prefix=''] - The prefix to use for flattened keys.
- * @returns {FlattenedObject} A flattened version of the input object.
+ * @returns A flattened version of the input object.
  * @example
  * const nestedObj = { a: 1, b: { c: 2, d: { e: 3 } } };
  * const flattenedObj = flatten(nestedObj);
  * // { 'a': 1, 'b.c': 2, 'b.d.e': 3 }
  */
-export function flatten(obj: Record<string, any>, prefix: string = ''): FlattenedObject {
-  return Object.keys(obj).reduce((acc: FlattenedObject, k) => {
+export function flatten<T extends Record<string, any>>(obj: T, prefix: string = ''): FlattenedObject<T> {
+  return Object.keys(obj).reduce((acc: FlattenedObject<T>, k) => {
     const pre = prefix.length ? prefix + '.' : '';
-    if (
-      typeof obj[k] === 'object' && 
-      obj[k] !== null && 
-      !Array.isArray(obj[k])
-    ) Object.assign(acc, flatten(obj[k], pre + k));
-    else acc[pre + k] = obj[k];
+    if (typeof obj[k] === 'object' && obj[k] !== null && !Array.isArray(obj[k])) {
+      Object.assign(acc, flatten(obj[k], pre + k));
+    } else {
+      (acc as any)[pre + k] = obj[k];
+    }
     return acc;
-  }, {})
-};
+  }, {} as FlattenedObject<T>);
+}
