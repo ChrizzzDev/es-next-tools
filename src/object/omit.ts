447fbe3ba87a94916fd4eda80e339f2a47c4ff73
ChrizzzDev <chrizdev4@gmail.com>
2025-01-09 10:34:40 -0600

    fix: Enhanced type definitions to accurately reflect the behavior and return types.

--- src/object/omit.ts
+++ src/object/omit.ts
@@ -1,9 +1,8 @@
 /**
  * Creates a new object with certain properties omitted.
  * @param {T} object - The source object.
- * @param {K[] | ((key: string, value: any) => boolean)} keysOrPredicate - An array of keys to omit or a predicate function.
- * @returns {Partial<T>} A new object with the specified keys omitted.
- * @template T, K
+ * @param {K[] | ((key: string, value: T[K]) => boolean)} keysOrPredicate - An array of keys to omit or a predicate function.
+ * @returns A new object with the specified keys omitted.
  * @example
  * const obj = { a: 1, b: 2, c: 3, d: 4 };
  * const result1 = omit(obj, ['b', 'd']);
@@ -13,7 +12,17 @@
  */
 export function omit<T extends Record<string, any>, K extends keyof T>(
   object: T,
-  keysOrPredicate: K[] | ((key: string, value: any) => boolean)
+  keysOrPredicate: K[]
+): Omit<T, K>;
+
+export function omit<T extends Record<string, any>, K extends keyof T>(
+  object: T,
+  keysOrPredicate: ((key: string, value: T[K]) => boolean)
+): Partial<T>;
+
+export function omit<T extends Record<string, any>, K extends keyof T>(
+  object: T,
+  keysOrPredicate: K[] | ((key: string, value: T[K]) => boolean)
 ): Partial<T> {
   const shouldOmit = Array.isArray(keysOrPredicate)
     ? (key: string) => keysOrPredicate.includes(key as K)
