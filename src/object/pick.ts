476eaef61ac6116459598e02cd98100bf74ce40b
ChrizzzDev <chrizdev4@gmail.com>
2025-01-09 10:40:05 -0600

    fix: Enhanced type definitions to accurately reflect the behavior and return types.

--- src/object/pick.ts
+++ src/object/pick.ts
@@ -1,9 +1,8 @@
 /**
  * Creates an object composed of the picked object properties.
  * @param {T} object - The source object.
- * @param {K[] | ((key: string, value: any) => boolean)} keysOrPredicate - An array of property names to pick or a predicate function.
+ * @param {K[] | ((key: string, value: T[K]) => boolean)} keysOrPredicate - An array of property names to pick or a predicate function.
  * @returns A new object with the picked properties.
- * @template T, K
  * @example
  * const obj = { a: 1, b: 2, c: 3, d: 4 };
  * const picked1 = pick(obj, ['a', 'c']); // { a: 1, c: 3 }
@@ -11,21 +10,31 @@
  */
 export function pick<T extends Record<string, any>, K extends keyof T>(
   object: T, 
-  keysOrPredicate: K[] | ((key: string, value: any) => boolean)
+  keysOrPredicate: K[]
+): Omit<T, K>;
+
+export function pick<T extends Record<string, any>, K extends keyof T>(
+  object: T, 
+  keysOrPredicate: ((key: string, value: T[K]) => boolean)
+): Partial<T>;
+
+export function pick<T extends Record<string, any>, K extends keyof T>(
+  object: T, 
+  keysOrPredicate: K[] | ((key: string, value: T[K]) => boolean)
 ) {
   const result = {} as Record<string, any>;
 
   if (Array.isArray(keysOrPredicate)) {
     for (const key of keysOrPredicate) {
       if (key in object) {
-        result[key as any] = object[key];
+        result[key as string] = object[key];
       }
     }
   } else if (typeof keysOrPredicate === 'function') {
     for (const key of Object.keys(object)) {
       const value = object[key];
       if (keysOrPredicate(key, value)) {
-        result[key as any] = value;
+        result[key] = value;
       }
     }
   }
