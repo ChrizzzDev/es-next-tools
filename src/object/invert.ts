187a6a21b0e17ba618817cda317bc59e34a9b07f
ChrizzzDev <chrizdev4@gmail.com>
2025-01-09 09:43:59 -0600

    fix: Enhanced type definitions to accurately reflect the inverted object structure.

--- src/object/invert.ts
+++ src/object/invert.ts
@@ -1,17 +1,37 @@
 /**
  * Inverts the keys and values of an object.
- * @param {Record<string, any>} object - The object to invert.
- * @returns {Record<string, any>} A new object with the original object's values as keys and keys as values.
+ * Currently, this function only supports PropertyKey type (string | number | symbol)
+ * @param {Readonly<T>} object - The object to invert.
+ * @returns A new object with the original object's values as keys and keys as values.
  * @example
  * const obj = { a: 1, b: 2, c: 3 };
  * const inverted = invert(obj);
  * // { '1': 'a', '2': 'b', '3': 'c' }
  */
-export function invert(object: Record<string, any>): Record<string, any>{
-  const result: Record<string, any> = {};
-  for (const [key, value] of Object.entries(object)) {
-    result[value] = key;
+export type Invertible = Record<PropertyKey, PropertyKey>;
+
+export type Inverted<T extends Invertible> = {
+  [K in keyof T as T[K]]: K;
+};
+
+export function invert<T extends Invertible>(object: Readonly<T>): Inverted<T> {
+  const _ = <P, Q>(
+    obj: Readonly<Record<string, P>>,
+    trans: (entry: [string, P]) => [string, Q]
+  ) => {
+    const result: Record<string, Q> = {};
+
+    for (const entry of Object.entries(obj)) {
+      const [key, value] = trans(entry);
+
+      result[key] = value;
+    }
+
+    return result;
   };
 
-  return result;
+  return _(object as Record<string, string>, ([key, value]) => [
+    value,
+    key,
+  ]) as Inverted<T>;
 }
